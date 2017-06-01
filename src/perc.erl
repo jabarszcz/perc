f7267c3622964db0745e8cc6ea011547ef19a505
Jean-Alexandre Barszcz <jalex_b@hotmail.com>
2017-06-01 15:48:29 -0400

    Add Erlang module generation

--- src/perc.erl
+++ src/perc.erl
@@ -23,7 +23,7 @@
 -spec main([string()]) -> no_return().
 main([Filename, RecordName]) ->
     Res = generate_codecs(Filename, [RecordName]),
-    io:format("~p~n", [Res]),
+    io:format("~s~n", [Res]),
     erlang:halt(0);
 main(_) ->
     io:format("usage ...~n"),
@@ -39,7 +39,8 @@ generate_codecs(Filename, RecordNames) ->
                          soname="sogenerated",
                          exported_records=RecordNames,
                          all_records=Deps,
-                         backends=["json"]}.
+                         backends=["json"]},
+    generate_erlang_module(Module).
 
 
 %%====================================================================
@@ -67,3 +68,46 @@ record_deps_(RecordDict, [Curr | ToVisit], Deps) ->
     record_deps_(RecordDict, NewToVisit, NewDeps);
 record_deps_(_, [], Deps) ->
     Deps.
+
+
+generate_erlang_module(Module = #nif_module{}) ->
+    ModuleAttr = ?S:attribute(
+                    ?S:atom(module),
+                    [?S:atom(Module#nif_module.name)]
+                   ),
+    Init = ?S:function(
+              ?S:atom(init),
+              [?S:clause(none, [?S:match_expr(
+                                   ?S:atom(ok),
+                                   ?S:application(
+                                      ?S:atom(erlang),
+                                      ?S:atom(load_nif),
+                                      [?S:string(Module#nif_module.soname),
+                                       ?S:integer(0)])
+                                  )])]
+             ),
+    Funcs = [?S:function( %% TODO spec
+               ?S:atom(Backend ++ "_" ++ Action ++ "_" ++ RecName),
+               [?S:clause([?S:underscore()],
+                          none,
+                          [?S:application(none, ?S:atom(throw),
+                                          [?S:atom(undefined)])])])
+             || RecName <- Module#nif_module.exported_records,
+                Action <- ["encode"], %% TODO
+                Backend <- Module#nif_module.backends ],
+    ExportAttrs = ?S:attribute(
+                     ?S:atom(export),
+                     [?S:list([?S:arity_qualifier(
+                                  ?S:function_name(Func),
+                                  ?S:integer(?S:function_arity(Func)))
+                      || Func <- [Init | Funcs] ])]
+                    ),
+    OnloadAttr = ?S:attribute(
+                    ?S:atom(on_load),
+                    [?S:tuple(
+                        [?S:function_name(Init),
+                         ?S:integer(?S:function_arity(Init))])]
+                   ),
+    Forms = ?S:revert_forms(
+               [ModuleAttr, ExportAttrs, OnloadAttr, Init | Funcs]),
+    forms:from_abstract(Forms).
