4deb6a7a555e9a201172433ac13553238431fb17
Jean-Alexandre Barszcz <jalex_b@hotmail.com>
2017-11-10 13:45:20 -0500

    Get rid of tabs

--- priv/perc_encode.h
+++ priv/perc_encode.h
@@ -6,76 +6,76 @@
 #include "erl_nif.h"
 
 struct encoder {
-	ErlNifBinary bin;
-	size_t index;
-	ErlNifEnv *env;
-	ERL_NIF_TERM undef_atom;
-	ERL_NIF_TERM true_atom;
-	ERL_NIF_TERM false_atom;
-	ERL_NIF_TERM opts;
+        ErlNifBinary bin;
+        size_t index;
+        ErlNifEnv *env;
+        ERL_NIF_TERM undef_atom;
+        ERL_NIF_TERM true_atom;
+        ERL_NIF_TERM false_atom;
+        ERL_NIF_TERM opts;
 };
 
 static inline
 bool encoder_init(ErlNifEnv *env, struct encoder *e,
-		   size_t size, ERL_NIF_TERM enc_opts)
+                   size_t size, ERL_NIF_TERM enc_opts)
 {
-	if (!enif_alloc_binary(size, &e->bin))
-		return false;
+        if (!enif_alloc_binary(size, &e->bin))
+                return false;
 
-	e->index = 0;
-	e->env = env;
-	e->opts = enc_opts;
+        e->index = 0;
+        e->env = env;
+        e->opts = enc_opts;
 
-	e->undef_atom = enif_make_atom(env, "undefined");
-	e->true_atom = enif_make_atom(env, "true");
-	e->false_atom = enif_make_atom(env, "false");
+        e->undef_atom = enif_make_atom(env, "undefined");
+        e->true_atom = enif_make_atom(env, "true");
+        e->false_atom = enif_make_atom(env, "false");
 
-	return true;
+        return true;
 }
 
 static inline
 ERL_NIF_TERM encoder_binary(struct encoder *e)
 {
-	enif_realloc_binary(&e->bin, e->index);
-	return enif_make_binary(e->env, &e->bin);
+        enif_realloc_binary(&e->bin, e->index);
+        return enif_make_binary(e->env, &e->bin);
 }
 
 static inline
 int ensure(struct encoder *e, size_t len)
 {
-	size_t size = e->bin.size;
+        size_t size = e->bin.size;
 
-	if (size - e->index >= len)
-		return 1;
+        if (size - e->index >= len)
+                return 1;
 
-	while (size - e->index < len && size * 2 > size)
-		size *= 2;
+        while (size - e->index < len && size * 2 > size)
+                size *= 2;
 
-	return enif_realloc_binary(&e->bin, size);
+        return enif_realloc_binary(&e->bin, size);
 }
 
 static inline
 size_t capacity(struct encoder *e)
 {
-	return e->bin.size - e->index;
+        return e->bin.size - e->index;
 }
 
 static inline
 char *get_ptr(struct encoder *e)
 {
-	return (char *) &e->bin.data[e->index];
+        return (char *) &e->bin.data[e->index];
 }
 
 static inline
 bool enc_buf(struct encoder *e, const char *buf, size_t len)
 {
-	if (!ensure(e, len))
-		return false;
+        if (!ensure(e, len))
+                return false;
 
-	memcpy(get_ptr(e), buf, len);
+        memcpy(get_ptr(e), buf, len);
 
-	e->index += len;
-	return true;
+        e->index += len;
+        return true;
 }
 
 #define ENC_LITERAL(e, str) enc_buf(e, str, sizeof(str)-1)
@@ -83,7 +83,7 @@ bool enc_buf(struct encoder *e, const char *buf, size_t len)
 static inline
 bool is_undefined(struct encoder *e, ERL_NIF_TERM term)
 {
-	return enif_is_identical(term, e->undef_atom);
+        return enif_is_identical(term, e->undef_atom);
 }
 
 #endif // _PERC_ENCODE_H_
--- priv/perc_json.hpp
+++ priv/perc_json.hpp
@@ -6,137 +6,137 @@
 
 template <class T>
 struct json_encoder {
-	static int encode(struct encoder *e, ERL_NIF_TERM term);
+        static int encode(struct encoder *e, ERL_NIF_TERM term);
 };
 
 template<>
 int json_encoder<Undefined>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_undefined(e, term);
+        return json_enc_undefined(e, term);
 }
 
 template<>
 int json_encoder<Integer>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_integer(e, term);
+        return json_enc_integer(e, term);
 }
 
 template<>
 int json_encoder<Float>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_float(e, term);
+        return json_enc_float(e, term);
 }
 
 template<>
 int json_encoder<Atom>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_atom(e, term);
+        return json_enc_atom(e, term);
 }
 
 template<>
 int json_encoder<Binary>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_binary(e, term);
+        return json_enc_binary(e, term);
 }
 
 template<>
 int json_encoder<String>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_string(e, term);
+        return json_enc_string(e, term);
 }
 
 template<>
 int json_encoder<Boolean>::encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_enc_boolean(e, term);
+        return json_enc_boolean(e, term);
 }
 
 template <typename T>
 struct json_encoder<List<T>> {
-	static int encode(struct encoder *e, ERL_NIF_TERM term) {
-		if (!enif_is_list(e->env, term))
-			return -1;
+        static int encode(struct encoder *e, ERL_NIF_TERM term) {
+                if (!enif_is_list(e->env, term))
+                        return -1;
 
-		size_t index = e->index;
-		if (!ENC_LITERAL(e, "["))
-			return -1;
+                size_t index = e->index;
+                if (!ENC_LITERAL(e, "["))
+                        return -1;
 
-		ERL_NIF_TERM head, tail=term;
-		bool is_first = true;
-		while(enif_get_list_cell(e->env, tail, &head, &tail)) {
-			if (is_first) {
-				is_first = false;
-			} else {
-				if (!ENC_LITERAL(e, ","))
-					goto fail;
-			}
-			if (json_encoder<T>::encode(e, head) < 0)
-				goto fail;
-		}
+                ERL_NIF_TERM head, tail=term;
+                bool is_first = true;
+                while(enif_get_list_cell(e->env, tail, &head, &tail)) {
+                        if (is_first) {
+                                is_first = false;
+                        } else {
+                                if (!ENC_LITERAL(e, ","))
+                                        goto fail;
+                        }
+                        if (json_encoder<T>::encode(e, head) < 0)
+                                goto fail;
+                }
 
-		if (!ENC_LITERAL(e, "]"))
-			goto fail;
-		return 1;
-	fail:
-		e->index = index;
-		return -1;
-	}
+                if (!ENC_LITERAL(e, "]"))
+                        goto fail;
+                return 1;
+        fail:
+                e->index = index;
+                return -1;
+        }
 };
 
 template<int i = 0,
-	 typename T,
-	 typename ... Ts>
+         typename T,
+         typename ... Ts>
 static
 int json_enc_tuple_rec(struct encoder *e, const ERL_NIF_TERM *fields)
 {
-	if (i) {
-		if (!ENC_LITERAL(e, ","))
-			return 0;
-	} else {
-		if (!ENC_LITERAL(e, "["))
-			return 0;
-	}
+        if (i) {
+                if (!ENC_LITERAL(e, ","))
+                        return 0;
+        } else {
+                if (!ENC_LITERAL(e, "["))
+                        return 0;
+        }
 
-	if (json_encoder<T>::encode(e, fields[i]) < 0)
-		return 0;
-	return json_enc_tuple_rec<i+1, Ts...>(e, fields);
+        if (json_encoder<T>::encode(e, fields[i]) < 0)
+                return 0;
+        return json_enc_tuple_rec<i+1, Ts...>(e, fields);
 }
 
 template<int i>
 static
 int json_enc_tuple_rec(struct encoder *e, const ERL_NIF_TERM *fields)
 {
-	return ENC_LITERAL(e, "]");
+        return ENC_LITERAL(e, "]");
 }
 
 template<typename ...Ts>
 struct json_encoder<Tuple<Ts...>> {
-	static int encode(struct encoder *e, ERL_NIF_TERM term) {
-		int arity;
-		const ERL_NIF_TERM *fields;
-		size_t index = e->index;
-		if (!enif_get_tuple(e->env, term, &arity, &fields))
-			return -1;
-		if (!json_enc_tuple_rec<0, Ts...>(e, fields)) {
-			e->index = index;
-			return -1;
-		}
-		return 1;
-	}
+        static int encode(struct encoder *e, ERL_NIF_TERM term) {
+                int arity;
+                const ERL_NIF_TERM *fields;
+                size_t index = e->index;
+                if (!enif_get_tuple(e->env, term, &arity, &fields))
+                        return -1;
+                if (!json_enc_tuple_rec<0, Ts...>(e, fields)) {
+                        e->index = index;
+                        return -1;
+                }
+                return 1;
+        }
 };
 
 template<typename T>
 static
 int json_enc_union_rec(struct encoder *e, ERL_NIF_TERM term) {
-	return json_encoder<T>::encode(e, term);
+        return json_encoder<T>::encode(e, term);
 }
 
 template<typename T1, typename T2, typename ...Ts>
 static
 int json_enc_union_rec(struct encoder *e, ERL_NIF_TERM term) {
-	if (json_encoder<T1>::encode(e, term) < 0)
-		return json_enc_union_rec<T2, Ts...>(e, term);
-	return 1;
+        if (json_encoder<T1>::encode(e, term) < 0)
+                return json_enc_union_rec<T2, Ts...>(e, term);
+        return 1;
 }
 
 template<typename... Ts>
 struct json_encoder<Union<Ts...>> {
-	static int encode(struct encoder *e, ERL_NIF_TERM term) {
-		return json_enc_union_rec<Ts...>(e, term);
-	}
+        static int encode(struct encoder *e, ERL_NIF_TERM term) {
+                return json_enc_union_rec<Ts...>(e, term);
+        }
 };
 
 template<trans_func enc, trans_func dec, typename T>
@@ -149,7 +149,7 @@ struct json_encoder<Function<enc, dec, T>> {
 template<typename T>
 static
 int json_encode(struct encoder *e, ERL_NIF_TERM term) {
-	return json_encoder<T>::encode(e, term);
+        return json_encoder<T>::encode(e, term);
 }
 
 #endif // _PERC_JSON_HPP_
